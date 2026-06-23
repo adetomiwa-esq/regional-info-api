f0f5eb3e396da9cac7e48a65c3b5e3de475d4c3e
Akinola Kehinde <khennyakin@gmail.com>
2026-06-23 09:28:47 +0100

    feat: update region model

--- server.js
+++ server.js
@@ -2,12 +2,24 @@ import express from "express";
 import { configDotenv } from "dotenv";
 import router from "./src/routes/regions.route.js";
 import connectDB from "./src/config/db.js";
+import cors from "cors";
 
 configDotenv();
 
 connectDB();
 
 const app = express();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://pop-tree-fe.vercel.app",
+      "https://www.tabeautypoptree.com",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }),
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
--- src/models/region.model.js
+++ src/models/region.model.js
@@ -3,19 +3,45 @@ import mongoose from "mongoose";
 const regionSchema = mongoose.Schema({
   population: {
     household_count: {
-      type: number,
+      type: Number,
       required: true,
     },
     gender: {
       male: {
-        type: number,
+        type: Number,
         required: true,
       },
       female: {
-        type: number,
+        type: Number,
         required: true,
       },
     },
+    age_groups: {
+      children: {
+        type: Number,
+        required: true,
+      },
+      youth: {
+        type: Number,
+        required: true,
+      },
+      elderly: {
+        type: Number,
+        required: true,
+      },
+    },
+  },
+  public_facilities: {
+    schools: [
+      {
+        name: {
+          type: String,
+        },
+        ownership: {
+          type: String, //can be government owned or private
+        },
+      },
+    ],
   },
 });
 
